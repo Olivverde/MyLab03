592dc47c422c84356e566ecb7f520529e5f60a6f
Olivverde19270 <oliver.milian00@gmail.com>
2020-02-02 18:43:28 -0600

    Second Activity

--- app/src/main/java/com/example/mylab03/MainActivity.kt
+++ app/src/main/java/com/example/mylab03/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.mylab03
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+//Key
+const val EXTRA_NAME = "com.example.mylab03.NAME"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,16 @@ class MainActivity : AppCompatActivity() {
      * Starts upper button activity
      */
     fun upperButtonActivity(view: View){
+
+        //Declaring editText
+        val editText = findViewById<EditText>(R.id.editText)
+        //Value
+        val name = editText.text.toString()
+        //Creating intent
+        val intent = Intent(this, activityUpperButton::class.java).apply {
+            putExtra(EXTRA_NAME,name)
+        }
+        startActivity(intent)
 
     }
 }
---
+++ app/src/main/java/com/example/mylab03/activityUpperButton.kt
@@ -0,0 +1,12 @@
+package com.example.mylab03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityUpperButton : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upper_button)
+    }
+}
