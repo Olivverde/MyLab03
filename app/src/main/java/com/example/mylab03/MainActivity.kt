ee6313de9688620b7039386f35514b82b1b0d228
Olivverde19270 <oliver.milian00@gmail.com>
2020-02-02 18:13:25 -0600

    Main Image

--- app/src/main/java/com/example/mylab03/MainActivity.kt
+++ app/src/main/java/com/example/mylab03/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.mylab03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Starts upper button activity
+     */
+    fun upperButtonActivity(view: View){
+
     }
 }
